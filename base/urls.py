27d172225cee40dea71f4f9d999f22edffa982b9
Na-Been <nlohani24@gmail.com>
2024-06-03 20:38:52 +0545

    theme added

--- base/urls.py
+++ base/urls.py
@@ -10,6 +10,9 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('room/<str:pk>/', views.room, name='room'),
     path('room-form/', views.roomForm, name='room-form'),
+    path('profile/<str:pk>', views.userProfile, name='profile'),
+
+
     path('update-room/<int:pk>', views.updateRoom, name='update-room'),
     path('delete-room/<int:pk>', views.deleteRoom, name='delete-room'),
     path('delete-chat/<str:pk>', views.deleteChat, name='delete-chat'),
